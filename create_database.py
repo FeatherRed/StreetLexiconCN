698820b7f6b96756852f2f10c48ebc0d55830b69
okuchiri <2489639088@qq.com>
2025-06-19 23:05:52 +0800

    创建数据库

--- create_database.py
+++ create_database.py
@@ -4,11 +4,12 @@ from requests_futures.sessions import FuturesSession
 import requests
 import os
 import time
+from sortedcontainers import SortedList
 
 base_id = 3600000000
 
 
-def create_database(city_id, city_name):
+def create_database(city_id, city_name, json_path):
     json_data = {}  # 用来保存该city_name的道路数据
     # 这个函数是用来迭代创建
     try:
@@ -29,12 +30,35 @@ def create_database(city_id, city_name):
         for element in data['elements']:
             if 'tags' in element and 'name' in element['tags']:
                 districts.append((element['tags']['name'], element['id']))
+            elif 'tags' in element and 'name:zh' in element['tags']:
+                districts.append((element['tags']['name'], element['id']))
+
+        query = f"""
+            [out:json];
+            area({city_id + base_id})->.searchArea;
+            (
+            way["highway"]["name"](area.searchArea);
+            );
+            out tags;
+        """
+        response = requests.get(url, params={'data': query})
+        data = response.json()
+        id_to_name = {}
+        road_in_city = set()
+        for element in data['elements']:
+            if 'tags' in element and 'name:zh' in element['tags']:
+                road_in_city.add(element['id'])
+                id_to_name[element['id']] = element['tags']['name:zh']
+            elif 'tags' in element and 'name' in element['tags']:
+                road_in_city.add(element['id'])
+                id_to_name[element['id']] = element['tags']['name']
 
         # 循环每个区
         session = FuturesSession(max_workers=16)
         all_futures = []
+        road_in_district = {}
         for district, district_id in districts:
-            print(f"正在处理 {city_name} 的 {district} 区")
+            #print(f"正在处理 {city_name} 的 {district} 区")
             json_data[district] = {}
             query = f"""
                 [out:json][timeout:90];
@@ -48,8 +72,35 @@ def create_database(city_id, city_name):
             data = response.json()
             # 得到街道名
             street = []
+            road_in_district2 = set()
+            query = f"""
+                [out:json][timeout:90];
+                area({district_id + base_id})->.searchArea;
+                (
+                way["highway"]["name"](area.searchArea);
+                );
+                out tags;
+            """
+            response = requests.get(url, params={'data': query})
+            data1 = response.json()
+            for element in data1['elements']:
+                if 'tags' in element and 'name:zh' in element['tags']:
+                    if element['id'] in road_in_city:
+                        road_in_city.discard(element['id'])
+                    road_in_district2.add(element['id'])
+                elif 'tags' in element and 'name' in element['tags']:
+                    if element['id'] in road_in_city:
+                        road_in_city.discard(element['id'])
+                    road_in_district2.add(element['id'])
+
+
+
+            road_in_district[district] = road_in_district2
             for element in data['elements']:
-                if 'tags' in element and 'name' in element['tags']:
+                if 'tags' in element and 'name:zh' in element['tags']:
+                    street.append((element['tags']['name:zh'], element['id']))
+
+                elif 'tags' in element and 'name' in element['tags']:
                     street.append((element['tags']['name'], element['id']))
 
             # 使用futures_session来异步请求街道数据
@@ -76,31 +127,45 @@ def create_database(city_id, city_name):
                     for element in data['elements']:
                         if 'tags' in element and 'name:zh' in element['tags']:
                             roads.add(element['tags']['name:zh'])
+                            road_in_district[district].discard(element['id'])
                         elif 'tags' in element and 'name' in element['tags']:
                             roads.add(element['tags']['name'])
+                            road_in_district[district].discard(element['id'])
 
                     json_data[district][street_name] = list(roads)
                 else:
                     return 0
             except Exception as e:
+                print('Error in')
                 return 0
     except Exception as e:
+        print("error")
         return 0
 
-    return json_data
+    json_data["roads"] = []
+    for ids in road_in_city:
+        json_data["roads"].append(id_to_name[ids])
+    json_data["roads"] = list(set(json_data["roads"]))
+    for district in json_data:
+        if district != "roads":
+            json_data[district]["roads"] = []
+            for ids in road_in_district[district]:
+                json_data[district]["roads"].append(id_to_name[ids])
+            json_data[district]["roads"] = list(set(json_data[district]["roads"]))
+    #return json_data
     # # 保存合并后的数据
-    # with open(json_path, 'w', encoding='utf-8') as f:
-    #     json.dump(existing_data, f, ensure_ascii=False, indent=4)
+    with open(json_path, 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
     # 示例调用
     with open("china_admin_data.json", 'r', encoding='utf-8') as f1:
         cities = json.load(f1)
-    province = "香港特别行政区"
+    province = "广东省"
     for city_name in cities["id"][province]:
         start_time = time.time()
-        #if city_name == "巴音郭楞蒙古自治州":
-        create_database(cities["id"][province][city_name], city_name, "data/"+city_name+".json")
+        if city_name == "汕尾市":
+            create_database(cities["id"][province][city_name], city_name, "data/"+city_name+".json")
         end_time = time.time()
         print(f"{city_name}数据创建完成，耗时: {end_time - start_time:.2f}秒")
